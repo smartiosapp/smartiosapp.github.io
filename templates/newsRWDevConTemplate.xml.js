1fa05b8fdfac6da7612b6ed961ba9e986beea378
方便咗系列 <97455815+smartiosapp@users.noreply.github.com>
2024-05-03 01:44:26 +0800

    Update newsRWDevConTemplate.xml.js

--- templates/newsRWDevConTemplate.xml.js
+++ templates/newsRWDevConTemplate.xml.js
@@ -30,7 +30,7 @@ var Template = function() { return `<?xml version="1.0" encoding="UTF-8" ?>
 	<menuItem videoURL="https://netleave.appspot.com/cnnnewsplayer">
 	<title>CNN</title>
 	</menuItem>
-	<menuItem videoURL="https://dai.google.com/linear/hls/pb/event/PwTJe4aZRvSBjbWY7_VSzA/stream/a879023a-8486-4e52-901a-cd70084003d9:TPE2/variant/ba9542af47a8524e66aeafceb2575c3d/bandwidth/1200000.m3u8">
+	<menuItem videoURL="https://dai.google.com/linear/hls/pb/event/PwTJe4aZRvSBjbWY7_VSzA/stream/5ab7f6a1-b499-4ecf-927b-afad3efb95d7:SIN/variant/ba9542af47a8524e66aeafceb2575c3d/bandwidth/1200000.m3u8">
 	<title>BLOOMBERG</title>
 	</menuItem>
 	</section>
